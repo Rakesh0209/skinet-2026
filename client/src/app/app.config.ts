3b5eda745b89ec8551c0a6abbe56d19017b73e96
Rakesh Rawat <rakesh_rawat02@outlook.com>
2026-09-08 09:29:46 +0530

    Add new images, implement header component, and enhance app layout with product listing

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,13 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import 'zone.js';
-
 import { routes } from './app.routes';
+import 'zone.js';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideZoneChangeDetection()
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideHttpClient()
   ]
 };
