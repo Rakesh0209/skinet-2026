d5f06774168f4014c2ebb40012fa63375e6d3c2c
Rakesh Rawat <rakesh_rawat02@outlook.com>
2026-09-13 14:31:26 +0530

    Implement shop feature with product listing, filtering, and pagination; add shop service and components

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,28 +1,15 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
-import { HttpClient } from '@angular/common/http';
-import { Product } from './shared/models/products';
-import { Pagination } from './shared/models/pagination';
+import { ShopComponent } from "./features/shop/shop.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  baseurl = "https://localhost:5001/api/";
-  private http = inject(HttpClient);
-  title = 'Skinet';
-  products: Product[] = [];
-
-  ngOnInit(): void {
-    this.http.get<Pagination<Product>>(this.baseurl + 'products').subscribe({
-      next: response => this.products = response.data,
-      error: error => console.log(error),
-      complete: () => console.log('Request completed')
-    });
-  }
-
+export class AppComponent {
+    title = 'Skinet';
 }
